cbf6fbe299857ef6267c1b1cdd8b82f21295d08c
Kinetics20 <kinetics20@gmail.com>
2025-04-10 22:21:11 +0200

    work out with sql select, insert, drop, update, set, delete

---
+++ dbs_sources/delete_4.sql
@@ -0,0 +1,39 @@
+# TINYINT 8bits
+# SMALLINT 16bits
+# MEDIUMINT 24bits
+# INT - 32bits -2**32, ,**32
+# BIGINT 64bits
+
+
+CREATE TABLE ActorSample (
+  actor_id SMALLINT unsigned NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(15) NOT NULL ,
+  last_name VARCHAR(25) NULL ,
+  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (actor_id)
+);
+
+SELECT *
+FROM ActorSample
+LIMIT 500;
+
+INSERT INTO ActorSample (first_name, last_name, last_update)
+SELECT first_name, last_name, last_update
+FROM actor;
+
+DELETE
+FROM ActorSample
+WHERE actor_id = 1;
+
+
+DELETE FROM ActorSample
+WHERE 1 = 1;
+
+TRUNCATE TABLE ActorSample;
+
+
+
+
+DROP TABLE ActorSample;
+
+

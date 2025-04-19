884a4e9a563edea412233e0b8edde6af4e4018b4
Kinetics20 <kinetics20@gmail.com>
2025-04-19 15:57:54 +0200

    practising SQL, procedures IN , OUT

--- workshop/procedure_intro.sql
+++ workshop/procedure_intro.sql
@@ -34,3 +34,67 @@ END //
 DELIMITER ;
 
 CALL    while_loop();
+
+
+DROP PROCEDURE while_loop;
+
+
+DELIMITER //
+
+    CREATE PROCEDURE concat_name(first_name VARCHAR(100), last_name VARCHAR(100))
+    BEGIN
+        DECLARE full_name VARCHAR(201);
+        SET full_name = CONCAT(first_name, ' ', last_name);
+        SELECT full_name;
+    END //
+
+DELIMITER ;
+
+
+DROP PROCEDURE concat_name;
+
+CALL concat_name('Mike', 'T');
+
+
+DELIMITER //
+
+    CREATE PROCEDURE add_language(lang VARCHAR(100))
+    BEGIN
+        INSERT INTO language (name)
+        VALUES (lang);
+
+        SELECT LAST_INSERT_ID();
+
+    END //
+
+DELIMITER ;
+
+
+DROP PROCEDURE add_language;
+
+
+CALL add_language('Korean');
+CALL add_language('Dutch');
+
+SELECT *
+FROM language
+LIMIT 500;
+
+DELIMITER //
+
+    CREATE PROCEDURE add_language(IN lang VARCHAR(100), OUT lang_id INT)
+    BEGIN
+        INSERT INTO language (name)
+        VALUES (lang);
+
+        SET lang_id = LAST_INSERT_ID();
+
+    END //
+
+DELIMITER ;
+
+
+CALL    add_language('Python', @lang_id);
+
+SELECT CONCAT('Last language id is: ', @lang_id);
+
